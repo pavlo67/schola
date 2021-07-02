60b22a613b600432d4d132802294558dd2e25bb8
pavlo <p.dzikovsky@gmail.com>
2021-07-02 14:02:26 +0300

    added sieve example code

---
+++ examples/e2/sieve.go
@@ -0,0 +1,148 @@
+package e2
+
+import (
+	"fmt"
+	"math"
+)
+
+// version 1: empty boilerplate -----------------------------------------
+
+func SieveV1(n int) []int {
+	return []int{}
+}
+
+// version 2: structured boilerplate with stubs -------------------------
+
+type sieve []bool
+
+func SieveV2(n int) []int {
+	//if n < 2 {
+	//	return []int{}
+	//}
+
+	sieveInitial := InitSieveV2(n)
+	sieveProcessed := ProcessNumbersFrom2ToSQRTNV2(sieveInitial)
+	primesFromSieve := GetPrimesFromSieveV2(sieveProcessed)
+
+	return primesFromSieve
+}
+
+func InitSieveV2(n int) sieve {
+	return sieve{}
+}
+
+func ProcessNumbersFrom2ToSQRTNV2(sieveInitial sieve) sieve {
+	return sieve{}
+}
+
+func GetPrimesFromSieveV2(sieveProcessed sieve) []int {
+	return []int{}
+}
+
+// version 3: functions are partially implemented ----------------------
+
+func SieveV3(n int) []int {
+	//if n < 2 {
+	//	return []int{}
+	//}
+
+	sieveInitial := InitSieveV3(n)
+	sieveProcessed := ProcessNumbersFrom2ToSQRTNV3(sieveInitial)
+	primesFromSieve := GetPrimesFromSieveV3(sieveProcessed)
+
+	return primesFromSieve
+}
+
+func InitSieveV3(n int) sieve {
+	if n < 2 {
+		return sieve{}
+	}
+
+	// we use n-1 because we need to get 1-element array for the first number == 2 and so on
+	sieveInitial := make(sieve, n-1)
+	for i := range sieveInitial {
+		sieveInitial[i] = true
+	}
+
+	return sieveInitial
+}
+
+func ProcessNumbersFrom2ToSQRTNV3(sieveInitial sieve) sieve {
+	return sieveInitial
+}
+
+func GetPrimesFromSieveV3(sieveProcessed sieve) []int {
+	fmt.Printf("final sieve to get primes: %#v\n\n", sieveProcessed)
+
+	if len(sieveProcessed) == 1 {
+		return []int{2}
+	} else if len(sieveProcessed) == 2 {
+		return []int{2, 3}
+	}
+
+	return []int{}
+}
+
+// version final: all functions are implemented -------------------------
+
+func Sieve(n int) []int {
+	//if n < 2 {
+	//	return []int{}
+	//}
+
+	sieveInitial := InitSieve(n)
+	sieveProcessed := ProcessNumbersFrom2ToSQRTN(sieveInitial)
+	primesFromSieve := GetPrimesFromSieve(sieveProcessed)
+
+	return primesFromSieve
+}
+
+func InitSieve(n int) sieve {
+	if n < 2 {
+		return sieve{}
+	}
+
+	// we use n-1 because we need to get 1-element array for the first number == 2 and so on
+	sieveInitial := make(sieve, n-1)
+	for i := range sieveInitial {
+		sieveInitial[i] = true
+	}
+
+	return sieveInitial
+}
+
+func ProcessNumbersFrom2ToSQRTN(sieveToProcess sieve) sieve {
+	// we could comment +1 here
+	n := len(sieveToProcess) + 1
+	if n < 2 {
+		return sieveToProcess
+	}
+	nMaxToProcess := math.Sqrt(float64(n))
+
+	for nToProcess := 2; float64(nToProcess) <= nMaxToProcess; nToProcess++ {
+		// we could comment -2 here
+		if !sieveToProcess[nToProcess-2] {
+			continue
+		}
+
+		for nToSetComposite := nToProcess + nToProcess; nToSetComposite <= n; nToSetComposite += nToProcess {
+			// we could comment -2 here
+			sieveToProcess[nToSetComposite-2] = false
+		}
+	}
+
+	return sieveToProcess
+}
+
+func GetPrimesFromSieve(sieveProcessed sieve) []int {
+	primesFromSieve := []int{}
+
+	for i := range sieveProcessed {
+		if sieveProcessed[i] {
+			// we use i+2 because the first i == 0 corresponds to the first prime number == 2 and so on
+			primesFromSieve = append(primesFromSieve, i+2)
+		}
+	}
+
+	return primesFromSieve
+}
---
+++ examples/e2/sieve_test.go
@@ -0,0 +1,61 @@
+package e2
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type TestCaseForPrimeNumbers struct {
+	NotGreaterThan int
+	PrimesExpected []int
+}
+
+var TestCasesForPrimeNumbers = []TestCaseForPrimeNumbers{
+	{-5, []int{}},
+	{-1, []int{}},
+	{0, []int{}},
+	{1, []int{}},
+	{2, []int{2}},
+	{3, []int{2, 3}},
+	{5, []int{2, 3, 5}},
+	{8, []int{2, 3, 5, 7}},
+	{25, []int{2, 3, 5, 7, 11, 13, 17, 19, 23}},
+	{55, []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53}},
+}
+
+// should fail (RED stage on RED-GREEN testing)
+func TestSieveV1(t *testing.T) {
+	for _, tc := range TestCasesForPrimeNumbers {
+		t.Logf("testing n = %d", tc.NotGreaterThan)
+		primesFound := SieveV1(tc.NotGreaterThan)
+		require.Equal(t, tc.PrimesExpected, primesFound)
+	}
+}
+
+// should fail (RED stage on RED-GREEN testing yet)
+func TestSieveV2(t *testing.T) {
+	for _, tc := range TestCasesForPrimeNumbers {
+		t.Logf("testing n = %d", tc.NotGreaterThan)
+		primesFound := SieveV2(tc.NotGreaterThan)
+		require.Equal(t, tc.PrimesExpected, primesFound)
+	}
+}
+
+// should fail (RED stage on RED-GREEN testing yet)
+func TestSieveV3(t *testing.T) {
+	for _, tc := range TestCasesForPrimeNumbers {
+		t.Logf("testing n = %d", tc.NotGreaterThan)
+		primesFound := SieveV3(tc.NotGreaterThan)
+		require.Equal(t, tc.PrimesExpected, primesFound)
+	}
+}
+
+// should pass (GREEN stage on RED-GREEN testing)
+func TestSieve(t *testing.T) {
+	for _, tc := range TestCasesForPrimeNumbers {
+		t.Logf("testing n = %d", tc.NotGreaterThan)
+		primesFound := Sieve(tc.NotGreaterThan)
+		require.Equal(t, tc.PrimesExpected, primesFound)
+	}
+}
